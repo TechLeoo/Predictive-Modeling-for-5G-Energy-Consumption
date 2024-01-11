ab2245ea297139af5ae5f99c3fdbcd88206fdd15
Onyiriuba Leonard <leonardleo110011@gmail.com>
2024-01-11 20:25:18 +0000

    Updates

--- model.py
+++ model.py
@@ -54,18 +54,22 @@ data = data.drop("Time", axis = 1)
         # Transform BS Column
 data = pd.get_dummies(data, drop_first = True, dtype = np.int8)
 
+        #  Data Binning 
+data["load"] = pd.cut(x = data["load"], bins = 30, labels = False)
+data.ESMODE = pd.cut(x = data.ESMODE, bins = 30, labels = False)        
+        
 # Further Data Preparation and Segregation
 x = data.drop("Energy", axis = 1)
 y = data.Energy
 
         # Feature Selection
-feature_tool = SelectKBest(score_func = f_regression, k = 10)
-new_x = feature_tool.fit_transform(x, y)
+# feature_tool = SelectKBest(score_func = f_regression, k = 10)
+# new_x = feature_tool.fit_transform(x, y)
 
-new_x = pd.DataFrame(new_x, columns = feature_tool.get_feature_names_out())
-feature_importance = pd.DataFrame({"Columns": feature_tool.feature_names_in_, "Feature Score": feature_tool.scores_})
+# new_x = pd.DataFrame(new_x, columns = feature_tool.get_feature_names_out())
+# feature_importance = pd.DataFrame({"Columns": feature_tool.feature_names_in_, "Feature Score": feature_tool.scores_})
 
-x_train, x_test, y_train, y_test = train_test_split(new_x, y, test_size = 0.2, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
 # Model Training
 regressor = XGBRegressor()
