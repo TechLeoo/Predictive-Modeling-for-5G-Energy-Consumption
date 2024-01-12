06f2b62e080675679da0f57d44f6fcd43fcfb0af
Onyiriuba Leonard <leonardleo110011@gmail.com>
2024-01-12 19:18:18 +0000

    Updates

---
+++ Data_Analysis.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from Initial_EDA import data
+
+# Exploratory Data Analysis
+data.info()
+data_head = data.head()
+data_tail = data.tail()
+data_descriptive_statistic = data.describe()
+data_more_desc_statistic = data.describe(include = "all")
+data_mode = data.mode()
+data_distinct_count = data.nunique()
+data_correlation_matrix = data.corr() 
+data_null_count = data.isnull().sum()
+data_total_null_count = data.isnull().sum().sum()
+data_hist = data.hist(figsize = (15, 10), bins = 10)
